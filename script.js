3570061f9708875e5787e2e0e39de7c83e013fdc
Yusuf <yusufkhanmd.1234@gmail.com>
2023-06-06 16:59:30 +0530

    fixed date

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var hours = document.getElementById("hours");
     const minutes = document.getElementById("minutes");
     const seconds = document.getElementById("seconds");
-    const date = document.getElementById("date");
-    const mmddyyyyBtn = document.getElementById("mmddyyy");
-    const ddmmyyyyBtn = document.getElementById("ddmmyyy");
     hours.innerHTML =
       currentTime.getHours() < 10
         ? "0" + currentTime.getHours()
@@ -23,42 +20,59 @@ document.addEventListener("DOMContentLoaded", function () {
         ? "0" + currentTime.getSeconds()
         : currentTime.getSeconds();
     ampm.innerHTML = hours >= 12 ? "PM" : "AM";
-    const mmddyyyyFormat = {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    };
-    const ddmmyyyyFormat = {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-    };
-
-    // Function to update the date display based on the selected format
-    function updateDateDisplay(format) {
-      date.innerHTML = currentTime.toLocaleDateString(undefined, format);
-    }
-
-    // Initial update based on default format (MM/DD/YYYY)
-    updateDateDisplay(mmddyyyyFormat);
-
-    // Event listeners for format buttons
-    mmddyyyyBtn.addEventListener("click", function () {
-      updateDateDisplay(mmddyyyyFormat);
-    });
-
-    ddmmyyyyBtn.addEventListener("click", function () {
-      updateDateDisplay(ddmmyyyyFormat);
-    });
-
     setTimeout(updateTime, 1000);
   }
+  function setDate(format) {
+    var today;
+    if (format === "ddmmyyyy") {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1; //January is 0!
+      var yyyy = today.getFullYear();
+
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+
+      today = dd + '/' + mm + '/' + yyyy;
+    } else if (format === "mmddyyyy") {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1; //January is 0!
+      var yyyy = today.getFullYear();
+
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+
+      today = mm + '/' + dd + '/' + yyyy;
+
+    }
+    document.getElementById("date").innerHTML = `${today}`;
+  }
+
+
+  document.getElementById("mmddyyyy").addEventListener("click", function () {
+    setDate("mmddyyyy")
+  });
+  document.getElementById("ddmmyyyy").addEventListener("click", function () {
+    setDate("ddmmyyyy")
+  });
+
+  setDate("ddmmyyyy");
   updateTime();
 });
 const clock = document.getElementById("clock");
 document.querySelector("a.btn").addEventListener("click", function () {
+  console.log("something happened")
   clock.classList.toggle("light");
   clock.classList.toggle("dark");
   document.body.style.background = clock.classList.contains("light")
